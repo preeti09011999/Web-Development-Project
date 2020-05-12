dddad05c4ba82e048027316471b18c03393b9578
Preeti <46680479+preeti09011999@users.noreply.github.com>
2020-05-12 20:23:05 +0530

    Update proj2.js

--- Dev Project/proj2.js
+++ Dev Project/proj2.js
@@ -4,7 +4,7 @@ let request = require("request");
 require("console.table");
 let url = process.argv[2];
 let fs = require("fs");
-
+let cName = process.argv[3];
 (async function () {
    try{
         // browser create => icognito mode,fullscreen
@@ -16,13 +16,13 @@ let fs = require("fs");
     //tab
     let tabs = await browser.pages();
     let tab = tabs[0];
-    
+    await tab.setDefaultNavigationTimeout(0); 
     // const options = {
     //     path :'images/corona.png',
     //     fullPage: true,
     //     omitBackground : true
     // };
-    tab.goto(url, {waituntil:"networkidle2"});
+    tab.goto(url, {waituntil:'load',timeout:0});
     // await t.setViewport({ width: 1280, height: 800 })
     // await tab.screenshot(options);
 
@@ -30,11 +30,13 @@ let fs = require("fs");
     // --------------------------------Total case-----------------------------------
     await tab.waitForSelector(".maincounter-number");
     const text = await tab.evaluate(() => Array.from(document.querySelectorAll('.maincounter-number span'), element => element.textContent));
+    console.log('------------------------------------------------------------------');
+    console.log('                    Covid19 World Status                     ');
     console.log("CoronaVirus Cases : " + text[0]);
     console.log("Deaths : " + text[1]);
     console.log("Recovered :" + text[2]);
 
-    --------------------------------Finding href----------------------------------
+    // --------------------------------Finding href----------------------------------
     const elementHandles = await tab.$$('a');
     const propertyJsHandles = await Promise.all(
       elementHandles.map(handle => handle.getProperty('href'))
@@ -42,54 +44,39 @@ let fs = require("fs");
     const hrefs2 = await Promise.all(
       propertyJsHandles.map(handle => handle.jsonValue())
     );
-    console.log(hrefs2);
-    await tab.goto(hrefs2[5]);
+  
+    // await tab.goto(hrefs2[5]);
+    // await tab.waitForNavigation({waituntil : "load"});
+    // await tab.screenshot({path : 'country_data.png'});
+    await tab.goto(url+ 'country/' + cName);
+    await tab.waitForSelector(".maincounter-number");
+    const cText = await tab.evaluate(() => Array.from(document.querySelectorAll('.maincounter-number span'), element => element.textContent));
+    console.log('------------------------------------------------------------------');
+    console.log('                    Covid19 '+cName + ' status                           ');
+    console.log("CoronaVirus Cases : " + cText[0]);
+    console.log("Deaths : " + cText[1]);
+    console.log("Recovered :" + cText[2]);
+    await tab.click('.flip_cases_front');
     await tab.waitForNavigation({waituntil : "load"});
-    
-
-    await tab.screenshot({path : 'country_data.png'});
+    console.log('------------------------------------------------------------------');
+    tab.screenshot({path : 'statistics.png'});
     let pUrl = hrefs2[4] + "#total-cases";
-    // console.log(pUrl);
     await tab.goto(pUrl);
     await tab.waitForNavigation({waituntil : "load",timeout : 0});
     await tab.screenshot({path : 'total-cases_graph.png'});
-    await tab.goto(url+ 'worldwide-graphs');
-    await tab.waitForNavigation({waituntil : "load",timeout : 0});
+    // await tab.goto(url+ 'worldwide-graphs');
+    // await tab.waitForNavigation({waituntil : "load",timeout : 0});
 
-    ----------------------active-cases--------------------------------------------
-    await tab.goto('url'+active-cases);
-    await tab.waitForNavigation({waituntil : "load",timeout:0});
-    await tab.screenshot({path : 'active-cases_graph.png'});
+    // ----------------------active-cases--------------------------------------------
+    // await tab.goto('url'+active-cases);
+    // await tab.waitForNavigation({waituntil : "load",timeout:0});
+    // await tab.screenshot({path : 'active-cases_graph.png'});
     
-    // -----------------------total-death--------------------------------------------
-    await tab.goto('url'+total-deaths);
-    await tab.waitForNavigation({waituntil : "load",timeout:0});
-    await tab.screenshot({path : 'total_deaths_graph.png'});
-    
-  //   -----------------------Using Cheerio to print table----------------------------
-  //   request(hrefs2[5],function (err,res,html) {
-  //   if(err === null && res.statusCode === 200){
-  //       fs.writeFileSync("index.html",html);
-  //           parseHtml(html);
-        
-  //   }else if(res.statusCode === 404){
-  //       console.log("Invalid URL");
-  //   }else{
-  //       console.log(err);
-  //       console.log(res.statusCode);
-  //   }
-  // })
-  // function parseHtml(html){
-  //   console.log("------------------------------------------------------------------------");
-  //   let $ = cheerio.load(html);
-  //   let itemWrapper = $("#main_table_countries_today.table.table-bordered.table-hover.main_table_countries.dataTable.no-footer");
-  //   let text = $(itemWrapper).text();
-
-  //   // console.log(text);
-  //   fs.writeFileSync('file.json',text);
-
-  }
- 
+    // // -----------------------total-death--------------------------------------------
+    // await tab.goto('url'+total-deaths);
+    // await tab.waitForNavigation({waituntil : "load",timeout:0});
+    // await tab.screenshot({path : 'total_deaths_graph.png'});
+  
 
    } catch(err){
        console.log(err);
